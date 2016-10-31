bc81d31d26c5f938f4f6c690c843292c089d1c3b
김진성 <tankattack@kookmin.ac.kr>
2016-10-31 17:19:26 +0900

    homework fin

---
+++ Homework01/app/src/main/java/kr/ac/kookmin/cs/homework01/GPSListener.java
@@ -0,0 +1,53 @@
+package kr.ac.kookmin.cs.homework01;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 2016-10-26.
+ */
+
+public class GPSListener implements LocationListener {
+    SQLiteDatabase db;
+    GoogleMap mMap;
+    ArrayList<LatLng> save;
+    PolylineOptions polylineOptions;
+
+    GPSListener(SQLiteDatabase db, GoogleMap mMap, ArrayList<LatLng> save){
+        this.db = db;
+        this.mMap = mMap;
+        this.save = save;
+    }
+
+    public void drawPolyline(){
+        polylineOptions = new PolylineOptions();
+        polylineOptions.color(Color.RED);
+        polylineOptions.width(5);
+        polylineOptions.addAll(save);
+        mMap.addPolyline(polylineOptions);
+    }
+
+    public void onLocationChanged(Location location) {
+        String s = "INSERT INTO Location (X, Y) VALUES(?,?);";
+        double x = location.getLatitude(), y = location.getLongitude();
+        db.execSQL(s, new Object[]{x, y});
+        save.add(new LatLng(x, y));
+        drawPolyline();
+        mMap.addCircle(new CircleOptions().center(new LatLng(x, y)).radius(10).strokeColor(Color.RED).fillColor(Color.BLUE));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(x, y)));
+    }
+    public void onProviderDisabled(String provider) {}
+    public void onProviderEnabled(String provider) {}
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+}
--- Homework01/app/src/main/java/kr/ac/kookmin/cs/homework01/MapsActivity.java
+++ Homework01/app/src/main/java/kr/ac/kookmin/cs/homework01/MapsActivity.java
@@ -1,6 +1,7 @@
 package kr.ac.kookmin.cs.homework01;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
@@ -30,7 +31,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ArrayList<LatLng> save = new ArrayList<LatLng>();
     PolylineOptions polylineOptions;
     GoogleMap mMap;
-    ZoomControls zoom;
 
 
     public void initialize(){
@@ -66,25 +66,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(test==null)test = new DBHelper(MapsActivity.this, "test_db", null, 1);
         db = test.getWritableDatabase();
         this.initialize();
-        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        final LocationListener mLocationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                String s = "INSERT INTO Location (X, Y) VALUES(?,?);";
-                double x=location.getLatitude(),y=location.getLongitude();
-                db.execSQL(s,new Object[]{x,y});
-                save.add(new LatLng(x,y));
-                drawPolyline();
-                mMap.addCircle(new CircleOptions().center(new LatLng(x,y)).radius(10).strokeColor(Color.RED).fillColor(Color.BLUE));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(x,y)));
-            }
-            public void onProviderDisabled(String provider) {}
-            public void onProviderEnabled(String provider) {}
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-        };
-        try{
-            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,6000,10,mLocationListener);
-            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 6000,10,mLocationListener);   //네트워크 제공자는 데이터가 안되서 사용을 못했습니다. 데이터를 사용해서 정확도는 떨어지지만 실내에서도 사용 가능하다는 점이 장점.
-        }catch(SecurityException ex){}
 
         if(save.size()!=0){
             mMap.moveCamera(CameraUpdateFactory.newLatLng(save.get(0)));
@@ -92,6 +73,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.610215, 126.997202)));
         }
         Toast.makeText(this, "DB에 있는 정보의 수 : "+Integer.toString(save.size()), Toast.LENGTH_SHORT).show();
-        //mMap.setMinZoomPreference(13);
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final LocationListener mLocationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                String s = "INSERT INTO Location (X, Y) VALUES(?,?);";
+                double x=location.getLatitude(),y=location.getLongitude();
+                db.execSQL(s,new Object[]{x,y});
+                save.add(new LatLng(x,y));
+                new MapsActivity().drawPolyline();
+                mMap.addCircle(new CircleOptions().center(new LatLng(x,y)).radius(1000).strokeColor(Color.RED).fillColor(Color.BLUE));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(x,y)));
+            }
+            public void onProviderDisabled(String provider) {}
+            public void onProviderEnabled(String provider) {}
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+        };
+        try{
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,60000,10,mLocationListener);
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 60000,10,mLocationListener);
+        }catch(SecurityException ex){}
     }
 }
